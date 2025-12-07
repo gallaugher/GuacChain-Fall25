64345e9a03544a0006c0c18d6f6b58e47614811a
gallaugher <john.gallaugher@bc.edu>
2025-12-07 16:54:20 -0500

    Initial Commit

---
+++ GuacChain/GuacChainApp.swift
@@ -0,0 +1,15 @@
+//  GuacChainApp.swift
+//  GuacChain
+//  Created by John Gallaugher on 12/7/25.
+//  YouTube.com/profgallaugher - gallaugher.bsky.social
+
+import SwiftUI
+
+@main
+struct GuacChainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
